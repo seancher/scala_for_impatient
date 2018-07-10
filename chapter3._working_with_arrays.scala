d16d45ceeae0790b6f6247ed837d854918a49201
Sergey Chernov <sergey.chernov@sanoma.com>
2018-07-10 16:33:01 +0300

    Done ex 3.6

--- chapter3._working_with_arrays.scala
+++ chapter3._working_with_arrays.scala
@@ -38,9 +38,22 @@ val arr = Array(1, -1, 0, 2, -2, 0, -3, 3, 0)
 arr.filter(_ > 0).union(arr.filter(_ <= 0))
 
 /*
-How do you compute the average of an Array[Double]?
+5. How do you compute the average of an Array[Double]?
 */
 
 var arr = Array(1.0, 2.0, 3.1, 5.9, 1.4)
+// option 1
 arr.reduce(_ + _) / arr.length
+// option 2
 arr.sum / arr.length
+
+/*
+6. How do you rearrange the elements of an Array[Int] so that they appear in reverse sorted
+order? How do you do the same with an ArrayBuffer[Int]?
+*/
+
+val arr = Array(1, 2, 3, -1, 0, -2, 0, -3, 0)
+val arrb = ArrayBuffer(1, 2, 3, -1, 0, -2, 0, -3, 0)
+
+arr.sorted.reverse
+arrb.sorted.reverse
