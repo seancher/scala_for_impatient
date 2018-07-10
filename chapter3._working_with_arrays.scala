3088865f3fc0bf6ce25b4c64b2e8cefcdf78b99b
Sergey Chernov <sergey.chernov@sanoma.com>
2018-07-10 14:35:21 +0300

    Done ex 3.2

--- chapter3._working_with_arrays.scala
+++ chapter3._working_with_arrays.scala
@@ -14,10 +14,17 @@ val arr = (for (i <- 0 until n)
 2, 3, 4, 5) becomes Array(2, 1, 4, 3, 5).
 */
 
+val arr = Array(1, 2, 3, 4, 5)
+for (i <- 1 until arr.length by 2) {
+val tmp = arr(i + 1) 
+arr(i + 1) = arr(i)
+arr(i) = tmp
+}
+
 /*
 3. Repeat the preceding assignment, but produce a new array with the swapped values. Use
 for/yield.
 */
 
-val arr = Array(1, 2, 3, 4)
+val arr = Array(1, 2, 3, 4, 5)
 for (i <- arr.indices) yield (if (i % 2 == 0) arr(i + 1) else arr(i - 1))
